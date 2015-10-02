18927e4737f4cd3647a17cbd44e331119597f204
jicorrea <j.ignacio_c@yahoo.com>
2015-10-01 21:09:16 -0300

    comit funciones Ajax/nexo en blanco comit formIngreso

---
+++ partes/formIngreso.php
@@ -0,0 +1,4 @@
+<form name="formIngreso" method="post">
+	<input type="text" placeholder="DNI xx.xxx.xxx" id="dni" name="dni" /><br><br>
+	<input type="submit" name="enviar" value="entrar" class="btn"/>
+</form>
